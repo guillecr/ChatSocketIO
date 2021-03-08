07d8977bdb61daed70c0ef9a7243a9b1a2faa08f
Guillermo <g.casas.r94@gmail.com>
2021-03-09 00:45:02 +0100

    v0.2
    Integración de mongo

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
-// Añadimos las dependencias
+// SERVIDOR
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http) // Montamos el Socket con el servidor HTML al cual está asociado Express
 const port = process.env.PORT || 3000
 
-var mensajes = []
 var usuarios = []
 var colores = ['red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange']
+
+
+// MONGOBD
+var mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://guilleStart:Sandrita28@cluster0.jxlyr.gcp.mongodb.net/chatIO?retryWrites=true&w=majority',
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    function (err) {
+        if (err) throw err;      
+        console.log('Conexión establecida con MongoDB');
+    }
+)
+
+var modelo = new mongoose.Schema({
+    time: Date,
+    text: String,
+    author: String,
+    color: String,
+});
+var MensajeModelo = mongoose.model('Mensajes', modelo );
 
 // Desordenamos la lista de colores 
 colores.sort(function(a,b) { return Math.random() > 0.5; })
 
 // Levantamos el escuchador por el puerto definido
 http.listen(port, () => {
-    console.log(`Escuchando por el puerto: ${port}`)
+    console.log(`Servidor levantado en el puerto: ${port}`)
 })
 
 // Creamos un metodo en Express que nos devolverá la pagina indicada
@@ -23,20 +41,21 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 });
 
-// Metodo cuando alguien se conecte 
-// socket será como nuestra sesión
+// CONEXIÓN CON USUARIO
 io.on('connection', (socket) => {
-    console.log((new Date()) + ' Conexión aceptada.')
+    console.log((new Date()) + ' Conexión aceptada')
     var userName = "Desconocido"
     var index = -1
     var userColor
     
     // Cargamos todos los mensajes
-    socket.emit('history',JSON.stringify(mensajes))
+    MensajeModelo.find(function(err,msgs){
+        socket.emit('history',JSON.stringify(msgs))
+    })
 
     // Definir nombre de usuario
     socket.on('user',(msg) => {
-        console.log(`Usuario llamado ${msg}`)
+        console.log(`Usuario identificado como: ${msg}`)
         userName = msg
         userColor = colores.shift()
         index = usuarios.push(msg) -1
@@ -52,18 +71,29 @@ io.on('connection', (socket) => {
             author: userName,
             color: userColor
         }
-        console.log('message: ' + msg)
+        console.log(`Mensaje de ${userName}: ${msg}`)
+        saveMensaje(objMsg)
         io.emit('chat message', JSON.stringify(objMsg))
-        mensajes.push(objMsg)
-
     })
 
     // Dexconexión del usuario
     socket.on('disconnect',()=>{
-        console.log('Usuario desconectado')
-        console.log(index)
+        console.log(`Usuario ${userName} desconectado`)
         usuarios.splice(index,1)
         colores.push(userColor)
         io.emit('users Chat',usuarios.join(" | "))
     })
 })
+
+function saveMensaje(msg){
+    var instancia = new MensajeModelo(
+        {
+            time:msg.time,
+            text:msg.text,
+            author:msg.author,
+            color:msg.color
+        }
+    )
+    instancia.save()
+    console.log("Mensaje guardado")
+}
