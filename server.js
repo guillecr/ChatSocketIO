110034d4ce61f663fe8f95ae3d8d98e5e519a296
Guillermo Casas <g.casas.r94@gmail.com>
2021-03-10 12:39:26 +0100

    v0.5.1

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ var colores = ['red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange']
 
 // ===========< MONGODB >==========
 // Conexión
-//mongoose.connect('mongodb+srv://guilleStart:Sandrita28@cluster0.jxlyr.gcp.mongodb.net/chatIO?retryWrites=true&w=majority',
-mongoose.connect('mongodb://localhost:27017',
+mongoose.connect('mongodb+srv://guilleStart:Sandrita28@cluster0.jxlyr.gcp.mongodb.net/chatIO?retryWrites=true&w=majority',
+//mongoose.connect('mongodb://localhost:27017',
     {useNewUrlParser: true, useUnifiedTopology: true},
     function (err) {
         if (err) throw err;      
